53f7632d372e97eb1a28cde42932220c7e61e4c1
ajautz@gmail.com <ajautz@gmail.com>
2022-07-15 10:52:22 -0400

    set up testing scripts

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,3 +4,12 @@ describe("Test GET/ launches", () => {
     expect(response).toBe(200);
   });
 });
+
+describe("Test POST/ launch", () => {
+  test("It should respond with 200 success", () => {
+
+  });
+
+  test("It should catch missing required properties", () => {});
+  test("It should catch missing dates", () => {});
+})
