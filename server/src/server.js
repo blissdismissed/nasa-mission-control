5ecb9f04f735067796059524229b6c9823759243
ajautz@gmail.com <ajautz@gmail.com>
2022-08-23 17:51:49 -0400

    install mongoose, enable connection to mongodbatlas cluster

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,20 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://Ish-Default:8iHSCKH51pGLww5u@cluster0.mym5r.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('connection is ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
