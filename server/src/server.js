dbce5a79dfb76bdae4a59b7cc73eb7b3170d45c9
ajautz@gmail.com <ajautz@gmail.com>
2022-08-23 18:13:35 -0400

    fix mongodb cluster vulnerability

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = 'mongodb+srv://Ish-Default:8iHSCKH51pGLww5u@cluster0.mym5r.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = `mongodb+srv://Ish-Default:${process.env.MONGODB_URL_CLUSTER}@cluster0.mym5r.mongodb.net/nasa?retryWrites=true&w=majority`;
 
 const server = http.createServer(app);
 
