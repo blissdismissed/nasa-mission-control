3eec410c5e478206e46b2a53127a79d97273c10d
ajautz@gmail.com <ajautz@gmail.com>
2022-09-06 10:53:47 -0400

    Update abort launch function to use mongoose and mongodb

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,7 +3,7 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map(); // local memory old way
+// const launches = new Map(); // local memory old way
 
 // let latestFlightNumber = 200; // local memory old way
 
@@ -21,8 +21,10 @@ const launch = {
 saveLaunch(launch);
 // launches.set(launch.flightNumber, launch);
 
-function existsLaunchWithId(launchId) {
-  return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+  return await launchesDatabase.findOne({
+    flightNumber: launchId
+  });
 }
 
 async function getLatestFlightNumber() {
@@ -76,11 +78,15 @@ async function scheduleNewLaunch(launch) {
   await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-  const aborted = launches.get(launchId);
-  aborted.upcoming = false;
-  aborted.success = false;
-  return aborted;
+async function abortLaunchById(launchId) {
+  const aborted = await launchesDatabase.updateOne({
+    flightNumber: launchId,
+  }, {
+    upcoming: false,
+    success: false,
+  });
+  console.log(aborted);
+  return aborted.matchedCount === 1 && aborted.modifiedCount === 1;
 }
 
 module.exports = {
