e9623a3c29d2e6af8d4e150afeed102d8c10c19c
ajautz@gmail.com <ajautz@gmail.com>
2022-09-16 11:43:30 -0400

    removed planet check on saveLaunch function

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -46,14 +46,13 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  })
+  // const planet = await planets.findOne({
+  //   keplerName: launch.target,
+  // })
 
-  if (!planet) {
-    throw new Error('No matching planet was found');
-  }
-
+  // if (!planet) {
+  //   throw new Error('No matching planet was found');
+  // }
   await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber
